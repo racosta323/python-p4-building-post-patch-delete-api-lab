44d83933bf201360f5b7abec4ee0d0c6a9d013a8
Rene <racosta323@gmail.com>
2024-03-15 16:20:54 -0500

    lab complete

--- server/app.py
+++ server/app.py
@@ -23,12 +23,24 @@ def bakeries():
     bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
     return make_response(  bakeries,   200  )
 
-@app.route('/bakeries/<int:id>')
+@app.route('/bakeries/<int:id>', methods=['GET', 'PATCH'])
 def bakery_by_id(id):
-
     bakery = Bakery.query.filter_by(id=id).first()
-    bakery_serialized = bakery.to_dict()
-    return make_response ( bakery_serialized, 200  )
+
+    if request.method == 'GET':
+        bakery_serialized = bakery.to_dict()
+        return make_response ( bakery_serialized, 200  )
+    elif request.method == 'PATCH':
+        for attr in request.form:
+            setattr(bakery, attr, request.form.get(attr))
+
+        db.session.add(bakery)
+        db.session.commit()
+
+        bakery_dict = bakery.to_dict()
+        response = make_response(bakery_dict, 200)
+        return response 
+    
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
@@ -45,5 +57,35 @@ def most_expensive_baked_good():
     most_expensive_serialized = most_expensive.to_dict()
     return make_response( most_expensive_serialized,   200  )
 
+@app.route('/baked_goods', methods=['POST'])
+def baked_goods():
+    good = BakedGood(
+        name=request.form.get('name'),
+        price=request.form.get('price')
+    )
+
+    db.session.add(good)
+    db.session.commit()
+
+    good_dict = good.to_dict()
+    response = make_response(good_dict,201)
+    return response
+
+@app.route('/baked_goods/<int:id>', methods=['DELETE'])
+def baked_good(id):
+    found_good=BakedGood.query.filter_by(id=id).first()
+    
+    db.session.delete(found_good)
+    db.session.commit()
+
+    response_body = {
+        "delete successful": True,
+        "message": "Baked good deleted."
+    }
+
+    response = make_response(response_body, 200)
+    return response 
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
